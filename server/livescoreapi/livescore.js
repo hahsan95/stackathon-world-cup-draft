68f7773e92f462f11ef9c4eb273469794e10e25b
Homum Ahsan <hahsan95@gmail.com>
2018-06-16 16:49:00 -0500

    adding an if else to get rid of repeated data

--- server/livescoreapi/livescore.js
+++ server/livescoreapi/livescore.js
@@ -4,29 +4,53 @@ const Games = require('../db/models/games')
 
 if (process.env.NODE_ENV !== 'production') require('../../secrets')
 
-let setGame = async (match) => {
-  for(let i = 0; i < match.length; i++) {
-    Games.create({ gameId: await match[i].id, homeTeam: await match[i].home_name, awayTeam: await match[i].away_name })
-  }
+// let setGame = async (match) => {
+//   for(let i = 0; i < match.length; i++) {
+//     Games.create({ gameId: await match[i].id, homeTeam: await match[i].home_name, awayTeam: await match[i].away_name })
+//   }
 
-}
+// }
 
-let getGamesFromAPI = async (league) => {
-  let response = await axios.get(`http://livescore-api.com/api-client/scores/history.json?key=${process.env.LIVESCORE_API_KEY}&secret=${process.env.LIVESCORE_API_SECRET}&league=${league}`)
+// let getGamesFromAPI = async (league) => {
+//   let response = await axios.get(`http://livescore-api.com/api-client/scores/history.json?key=${process.env.LIVESCORE_API_KEY}&secret=${process.env.LIVESCORE_API_SECRET}&league=${league}`)
+//   try {
+//     let matches = response.data.data.match
+//     setGame(matches)
+//     console.log(`*** league ${league} synced ***`)
+
+//   } catch (err) {
+//     console.log('caught an error', err)
+//   }
+// }
+
+// let setEveryLeague = async () => {
+//   for (let i = 793; i <= 800; i++) {
+//     await getGamesFromAPI(i)
+//   }
+// }
+// setEveryLeague()
+
+// /*
+// Checking For New Scores Every Hour
+// */
+
+// let checkingEveryHour = () => {
+//   console.log('Checking For Newly Completed Matches')
+//   setEveryLeague()
+// }
+// setInterval(checkingEveryHour, 60*60*1000)
+
+let poop = async () => {
   try {
-    let matches = response.data.data.match
-    setGame(matches)
-    console.log(`*** league ${league} synced ***`)
-
+    let poopy = await Games.findOne({
+      where: {
+        gameId: 943970
+      }
+    })
+    console.log(poopy.id)
   } catch (err) {
-    console.log('caught an error', err)
+    console.log('Caught an error: ', err)
   }
 }
 
-let setEveryLeague = async () => {
-  for (let i = 793; i <= 800; i++) {
-    await getGamesFromAPI(i)
-  }
-}
-setEveryLeague()
-
+poop()
