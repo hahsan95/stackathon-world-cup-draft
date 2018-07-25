810f423c60b92bf9d52afa4037a3256ae7e27e66
Homum Ahsan <hahsan95@gmail.com>
2018-07-24 23:35:20 -0500

    got goals working

--- server/db/models/goals.js
+++ server/db/models/goals.js
@@ -2,29 +2,13 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Goals = db.define('goals', {
-  byRank: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
   byName: {
     type: Sequelize.STRING,
-    allowNull: false
-  },
-  onRank: {
-    type: Sequelize.INTEGER,
     allowNull: false
   },
   onName: {
     type: Sequelize.STRING,
     allowNull: false
-  },
-  pointsAwarded: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  recorded : {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false
   }
 })
 
--- server/newscoreapi/pointcounter.js
+++ server/newscoreapi/pointcounter.js
@@ -4,7 +4,7 @@ const Countries = require('../db/models/countries')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
-let pointCounter = async () => {
+let goalCounterHome = async () => {
   let gamesToAdd = await Games.findAll()
   for(let i = 0; i < gamesToAdd.length; i++){
     let homeTeams = await Countries.findAll({
@@ -15,21 +15,48 @@ let pointCounter = async () => {
         }
       }
     })
+
     if(homeTeams.length && gamesToAdd[i].score1 > 0){
-      console.log(homeTeams[0].name, gamesToAdd[i].score1)
+      let score = gamesToAdd[i].score1
+      let byName = homeTeams[0].name
+      let onName = gamesToAdd[i].awayTeam
+      let userId = homeTeams[0].userId
+      // console.log(byName, onName, 'for: ' + userId, 'score: ' + score)
+      for(let i = 0; i < score; i++){
+        await  Goals.create({
+          byName: byName, onName: onName, userId: userId
+        })
+      }
     }
-    // let awayTeams = await Countries.findAll({
-    //   where: {
-    //     name: gamesToAdd[i].awayTeam,
-    //     userId: {
-    //       [Op.ne]: null
-    //     }
-    //   }
-    // })
-    // if(awayTeams.length){
-    //   console.log(awayTeams[0].name)
-    // }
   }
 }
 
-pointCounter();
+let goalCounterAway = async () => {
+  let gamesToAdd = await Games.findAll()
+  for(let i = 0; i < gamesToAdd.length; i++){
+    let awayTeams = await Countries.findAll({
+      where: {
+        name: gamesToAdd[i].awayTeam,
+        userId: {
+          [Op.ne]: null
+        }
+      }
+    })
+
+    if(awayTeams.length && gamesToAdd[i].score2 > 0){
+      let score = gamesToAdd[i].score2
+      let byName = awayTeams[0].name
+      let onName = gamesToAdd[i].homeTeam
+      let userId = awayTeams[0].userId
+      // console.log(byName, onName, 'for: ' + userId, 'score: ' + score)
+      for(let x = 0; x < score; x++){
+        await  Goals.create({
+          byName: byName, onName: onName, userId: userId
+        })
+      }
+    }
+  }
+}
+
+// goalCounterHome();
+// goalCounterAway();
