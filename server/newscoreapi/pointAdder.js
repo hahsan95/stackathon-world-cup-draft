566a8fd4eda737a41198c8237fa10359f6ea536c
Homum Ahsan <hahsan95@gmail.com>
2018-07-25 04:10:52 -0500

    got scores per goal

--- server/newscoreapi/pointAdder.js
+++ server/newscoreapi/pointAdder.js
@@ -1,2 +1,31 @@
-// const Goals = require('../db/models/goals')
-// const Users = require('../db/models/users')
+const Goals = require('../db/models/goals')
+const User = require('../db/models/user')
+
+let pointCounter = async () => {
+  let goals = await Goals.findAll()
+  for(let i = 0; i < goals.length; i++){
+    if(goals[i].byRank > goals[i].onRank){
+      let score = 5 + (goals[i].byRank - goals[i].onRank)
+      await Goals.update({
+        pointsAwarded: score
+      }, {
+        where: {
+          byName: goals[i].byName,
+          onName: goals[i].onName
+        }
+      })
+    } else {
+      let score = 5
+      await Goals.update({
+        pointsAwarded: score
+      }, {
+        where: {
+          byName: goals[i].byName,
+          onName: goals[i].onName
+        }
+      })
+    }
+  }
+}
+
+pointCounter()
