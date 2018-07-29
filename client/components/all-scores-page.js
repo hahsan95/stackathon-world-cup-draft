ae55aaced10fdf3420f81acf952cb2a1bc741fa2
Homum Ahsan <hahsan95@gmail.com>
2018-07-29 00:59:16 -0500

    added table

--- client/components/all-scores-page.js
+++ client/components/all-scores-page.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getScoresListThunk } from '../store'
+import { Header, Grid, Segment, Table, Container, Image } from 'semantic-ui-react'
+
+const style = {
+  h1: {
+    marginTop: '3em',
+  },
+  h2: {
+    margin: '4em 0em 2em',
+  },
+  h3: {
+    marginTop: '2em',
+    padding: '2em 0em',
+  },
+  last: {
+    marginBottom: '300px',
+  },
+}
 
 class AllScores extends Component {
 
@@ -16,9 +33,40 @@ class AllScores extends Component {
     let scores = this.props.scoresList.sort((a, b) => a.points < b.points)
     return(
       <div>
-          {
-            scores.map(score => <h1 key={score.id}>{score.name}: {score.points}</h1>)
-          }
+          {/* {
+            scores.map(score => <h3 key={score.id}>{score.name}: {score.points}</h3>)
+          } */}
+        <Header as='h3' content='Final Scores Table' style={style.h3} textAlign='center' />
+        <Container>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Score</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+
+              {
+                scores.map(score => <Table.Row>
+                  <Table.Cell>
+                  <Header as='h4' image>
+                    {/* <Image rounded size='mini' src='/images/wireframe/square-image.png' /> */}
+                    <Header.Content>
+                      {score.name}
+                      <Header.Subheader>{score.email}</Header.Subheader>
+                    </Header.Content>
+                  </Header>
+                  </Table.Cell>
+                  <Table.Cell>{score.points}</Table.Cell>
+                </Table.Row>)
+              }
+
+
+            </Table.Body>
+          </Table>
+        </Container>
       </div>
     )
   }
